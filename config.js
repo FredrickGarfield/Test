3d90d824c86a1672f9100b3ac0ae5f53d377ac22
Ryan Wilson <92859295+MovByte@users.noreply.github.com>
2023-12-16 01:53:10 -0500

    t

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 /*global Ultraviolet*/
 self.__uv$config = {
-    prefix: '/api/',
+    prefix: '/gh/ProxyHaven/Test@master/api/',
     bare: 'https://decimals.mountzion.nsw.edu.au/bare/',
     encodeUrl: Ultraviolet.codec.plain.encode,
     decodeUrl: Ultraviolet.codec.plain.decode,
