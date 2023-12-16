41365f4b8f487e252babe9d8d9e9efe4a2bee901
Ryan Wilson <92859295+MovByte@users.noreply.github.com>
2023-12-16 01:30:35 -0500

    t

---
+++ sw3.js
@@ -0,0 +1,9 @@
+importScripts('bundle.js');
+importScripts('config.js');
+importScripts('the.js');
+
+const sw = new UVServiceWorker();
+
+//self.addEventListener('fetch', (event) => event.respondWith(new Response(event.request.url || "hello world")));
+
+self.addEventListener('fetch', (event) => event.respondWith(sw.fetch(event)));
